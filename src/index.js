8acfe581bb838110980c3ec5ab0fc02f55c814bb
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-07-06 21:11:20 -0500

    pug implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import Debug from 'debug';
-
+import path from 'path';
 import variables from './configs/env';
 
 const debug = Debug('app:index');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(variables.port, () => {
   debug(`Server is running on port ${variables.port}`);
